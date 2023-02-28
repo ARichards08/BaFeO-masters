92fc75c87f9e82b4f5053c5ff3ea959292964fa4
ARichards08 <88149323+ARichards08@users.noreply.github.com>
2023-02-28 14:26:25 +0000

    Counting the original file too

--- counting/match.sh
+++ counting/match.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 awk -F '  ' 'NR>138 {print $7}' SrFeO-test.ucf | sort | uniq -c | sort -nr
+
+awk -F '\t' 'NR>138 {print $7}' BaFeO-original.ucf | sort | uniq -c | sort -nr
+
+awk -v OFS='\t' '$7 ~ /-5.0068125e-21/ {print $2, $3}' BaFeO-original.ucf
